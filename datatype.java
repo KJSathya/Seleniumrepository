f877ffa517d6495e2e08c540b0e5af08708a7808
KJSathya <142685679+KJSathya@users.noreply.github.com>
2023-08-20 00:07:10 -0700

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class datatype {
+	public static void main(String[] args) {
+		
+		int year = 1989;
+		String detail="born on";
+		boolean	value= true;
+		char va='A';
+		System.out.println("the member "+detail+" "+"The year"+year );
+		System.out.println("the ans" +value);
+		System.out.println("the value" +va);
+		
+		}
+		
+	}
+	
+	
+	
+	
+	
+
